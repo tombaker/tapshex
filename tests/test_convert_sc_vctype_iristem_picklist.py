7684f13662f52142578f9be38d6190c1a6080981
Tom Baker <tom@tombaker.org>
2021-07-18 06:48:49 +0200

    iristem - test passes

---
+++ tests/test_convert_sc_vctype_iristem_picklist.py
@@ -0,0 +1,48 @@
+"""Convert statement constraints of value constraint type "picklist"."""
+
+def _convert_sc_vct_iristem_picklist_to_tc(sc):
+    """@@@"""
+    if sc.get("valueConstraintType") == "iristem":
+        tc_predicate = sc.get("propertyID")
+        valueexpr_values = list()
+        for iristem in sc.get("valueConstraint"):
+            nc_value = dict()
+            nc_value["type"] = "IriStem"
+            nc_value["stem"] = iristem
+            valueexpr_values.append(nc_value)
+        tc = dict()
+        tc["type"] = "TripleConstraint"
+        tc["predicate"] = tc_predicate
+        tc["valueExpr"] = dict()
+        tc["valueExpr"]["type"] = "NodeConstraint"
+        tc["valueExpr"]["values"] = valueexpr_values
+    return tc
+
+def test_convert_sc_vct_iristem_picklist_to_nc():
+    """@@@"""
+    input_dict = {
+      "propertyID": "http://purl.org/dc/terms/subject",
+      "valueConstraint": [
+          "https://id.loc.gov",
+          "http://loc.nal.usda.gov",
+      ],
+      "valueConstraintType": "iristem"
+    }
+    output_dict = {
+      "type": "TripleConstraint",
+      "predicate": "http://purl.org/dc/terms/subject",
+      "valueExpr": {
+        "type": "NodeConstraint",
+        "values": [
+          {
+            "type": "IriStem",
+            "stem": "https://id.loc.gov"
+          },
+          {
+            "type": "IriStem",
+            "stem": "http://loc.nal.usda.gov"
+          }
+        ]
+      }
+    }
+    assert _convert_sc_vct_iristem_picklist_to_tc(input_dict) == output_dict
