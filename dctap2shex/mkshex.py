e40727264ec1616c93d29957c24d12a095039cce
Tom Baker <tom@tombaker.org>
2021-07-18 21:34:59 +0200

    mkshex.py - update class names

--- dctap2shex/mkshex.py
+++ dctap2shex/mkshex.py
@@ -1,7 +1,8 @@
-"""Convert list of CSVShape instances into ShEx Schema."""
+"""Convert list of TAPShapeX instances into ShEx Schema."""
 
 from typing import Union, List, Optional
 from jsonasobj import as_json, loads
+from .xclasses import TAPShapeX, TAPStatementConstraintX
 from ShExJSG import Schema
 from ShExJSG.ShExJ import (
     Shape,
@@ -13,7 +14,7 @@ from ShExJSG.ShExJ import (
 )
 
 
-def get_node_constraint(csv_tc: CSVTripleConstraint) -> Optional[shapeExpr]:
+def get_node_constraint(tap_sc: TAPStatementConstraintX) -> Optional[shapeExpr]:
     """Generate ShEx node constraint from CSV triple constraint if necessary."""
 
     rval = None
@@ -22,26 +23,26 @@ def get_node_constraint(csv_tc: CSVTripleConstraint) -> Optional[shapeExpr]:
         return rval if rval else NodeConstraint()
 
     nc = get_nc()
-    if csv_tc.valueNodeType:
+    if tap_sc.valueNodeType:
         #  pattern = jsg.JSGPattern(r'(iri)|(bnode)|(nonliteral)|(literal)')
-        nc.nodeKind = csv_tc.valueNodeType.lower()
-#    if csv_tc.valueConstraint:
-#        get_nc().values = [csv_tc.valueConstraint]
-#    if csv_tc.valueConstraintType:
-#        get_nc().datatype = IRIREF(csv_tc.valueConstraintType)
-#    if csv_tc.valueShape:
+        nc.nodeKind = tap_sc.valueNodeType.lower()
+#    if tap_sc.valueConstraint:
+#        get_nc().values = [tap_sc.valueConstraint]
+#    if tap_sc.valueConstraintType:
+#        get_nc().datatype = IRIREF(tap_sc.valueConstraintType)
+#    if tap_sc.valueShape:
 #        if rval:
 #            raise ValueError(
 #                "Statement constraint cannot have both "
 #                "a NodeConstraint and a ValueConstraint"
 #            )
-#        return IRIREF(csv_tc.valueShape)
+#        return IRIREF(tap_sc.valueShape)
 #    return rval
     return nc
 
 
-def add_triple_constraint(shape: Shape, csv_tc: CSVTripleConstraint) -> None:
-    """Interpret CSV triple constraint and add shapeExpr to shape."""
+def add_triple_constraint(shape: Shape, tap_sc: TAPStatementConstraintX) -> None:
+    """Interpret TAP statement constraint and add shapeExpr to shape."""
 
     # pylint: disable=invalid-name
     # two-letter variable names do not conform to snake-case naming style
@@ -49,11 +50,11 @@ def add_triple_constraint(shape: Shape, csv_tc: CSVTripleConstraint) -> None:
     # typing.List[typing.Union["EachOf", "OneOf", "TripleConstraint", typing.Union[str, str]]]
     ts = TripleConstraint(
         # Why does a triple constraint need to have a label?
-        # id=csv_tc.prop_label,
-        predicate=IRIREF(csv_tc.propertyID),
-        min=1 if csv_tc.mandatory else 0,
-        max=-1 if csv_tc.repeatable else 1,
-        valueExpr=get_node_constraint(csv_tc),
+        # id=tap_sc.prop_label,
+        predicate=IRIREF(tap_sc.propertyID),
+        min=1 if tap_sc.mandatory else 0,
+        max=-1 if tap_sc.repeatable else 1,
+        valueExpr=get_node_constraint(tap_sc),
     )
     if shape.expression:
         if isinstance(shape.expression, TripleConstraint):
@@ -64,13 +65,13 @@ def add_triple_constraint(shape: Shape, csv_tc: CSVTripleConstraint) -> None:
         shape.expression = ts
 
 
-def mkshex(shapes: Union[CSVShape, List[CSVShape]]) -> Schema:
+def mkshex(shapes: Union[TAPShapeX, List[TAPShapeX]]) -> Schema:
     """Convert list of csv2shape Shapes to ShExJSG Schema object."""
 
     # pylint: disable=invalid-name
     # One- and two-letter variable names do not conform to snake-case naming style
 
-    if isinstance(shapes, CSVShape):
+    if isinstance(shapes, TAPShapeX):
         shapes = [shapes]
     schema_shexjsg = Schema()
     for s in shapes:
@@ -81,8 +82,8 @@ def mkshex(shapes: Union[CSVShape, List[CSVShape]]) -> Schema:
             else:
                 schema_shexjsg.start = shape_id
         shape = Shape(id=shape_id)
-        for csv_tc in s.tc_list:
-            add_triple_constraint(shape, csv_tc)
+        for tap_sc in s.tc_list:
+            add_triple_constraint(shape, tap_sc)
         if not schema_shexjsg.shapes:
             schema_shexjsg.shapes = [shape]
         else:
