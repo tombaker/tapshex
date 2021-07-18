af484e1c278e9d708db718f93d1b8143efaf38a4
Tom Baker <tom@tombaker.org>
2021-07-19 01:20:53 +0200

    mand/repeat tests passing

--- tests/test_convert_sc_mandatory_to_min.py
+++ tests/test_convert_sc_mandatory_to_min.py
@@ -1,14 +1,6 @@
 """Mandatory / repeatable."""
 
-def _convert_sc_mandatory_to_sc_min(sc):
-    """@@@"""
-    if sc.get("mandatory").lower() == "true":
-        sc["min"] = 1 
-        del sc["mandatory"]
-    else:
-        sc["min"] = 0
-        del sc["mandatory"]
-    return sc
+from dctap2shex.utils import _convert_sc_mandatory_to_sc_min, _convert_sc_repeatable_to_sc_min
 
 def test_convert_sc_mandatory_to_sc_min_true():
     """@@@"""
