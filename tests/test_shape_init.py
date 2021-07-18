4f1cfda7ba4a729702f1bac70849bc1926815b1b
Tom Baker <tom@tombaker.org>
2021-07-18 07:31:28 +0200

    improve shape initialization

--- tests/test_shape_init.py
+++ tests/test_shape_init.py
@@ -1,10 +1,13 @@
-"""Convert statement constraints of value constraint type "picklist"."""
+"""Initializes Python dictionary of type "Shape" with default "EachOf"."""
 
 def _mkshape(shape_identifier):
     """Makes a shape identifier with shapeID as @id."""
     shape = dict()
     shape["type"] = "Shape"
     shape["id"] = shape_identifier 
+    shape["expression"] = dict()
+    shape["expression"]["type"] = "EachOf"
+    shape["expression"]["expressions"] = list()
     return shape
 
 def test_convert_picklist_of_two_literals():
@@ -12,7 +15,11 @@ def test_convert_picklist_of_two_literals():
     input_iri = "http://example.org/book" 
     output_dict = {
       "type": "Shape",
-      "id": "http://example.org/book" 
+      "id": "http://example.org/book",
+      "expression": {
+        "type": "EachOf",
+        "expressions": []
+      }
     }
     assert _mkshape(input_iri) == output_dict
 
