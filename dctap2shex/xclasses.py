80f75bdfe990c4accfb540ae5c68970967b1498f
Tom Baker <tom@tombaker.org>
2021-07-18 21:34:14 +0200

    xclasses - move _start_closed_have_supported_boolean_values to "later"

--- dctap2shex/xclasses.py
+++ dctap2shex/xclasses.py
@@ -9,6 +9,9 @@ from dctap.utils import is_uri_or_prefixed_uri
 @dataclass
 class TAPStatementConstraintX(TAPStatementConstraint):
     """A statement constraint, its fields extended from 'dctap' for ShEx."""
+    # pylint: disable=invalid-name
+    # True that propertyID, etc, do not conform to snake-case naming style.
+
     min: str = ""
     max: str = ""
 
@@ -16,7 +19,6 @@ class TAPStatementConstraintX(TAPStatementConstraint):
 @dataclass
 class TAPShapeX(TAPShape):
     """A shape, its fields extended from 'dctap' for ShEx."""
-
     # pylint: disable=invalid-name
     # True that propertyID, etc, do not conform to snake-case naming style.
 
@@ -25,37 +27,6 @@ class TAPShapeX(TAPShape):
 
     def validate2(self, config_dict=None):
         """Normalize values where required."""
-        self._start_closed_have_supported_boolean_values()
+        # self._start_closed_have_supported_boolean_values()
         return True
 
-    def _start_closed_have_supported_boolean_values(self):
-        """Booleans take true/false (case-insensitive) or 1/0, default None."""
-        valid_values_for_true = ["true", "1"]
-        valid_values_for_false = ["false", "0"]
-        valid_values = valid_values_for_true + valid_values_for_false + [None]
-        # pylint: disable=singleton-comparison
-        if self.closed != None:
-            # breakpoint(context=5)
-            mand = self.closed.lower()
-            if mand not in valid_values and mand != "":
-                self.sc_warnings[
-                    "closed"
-                ] = f"{repr(self.closed)} is not a supported Boolean value."
-            if mand in valid_values_for_true:
-                self.closed = True
-            elif mand in valid_values_for_false:
-                self.closed = False
-
-        if self.start != None:
-            # breakpoint(context=5)
-            repeat = self.start.lower()
-            if repeat not in valid_values and repeat != "":
-                self.sc_warnings[
-                    "start"
-                ] = f"{repr(self.start)} is not a supported Boolean value."
-            if repeat in valid_values_for_true:
-                self.start = True
-            elif repeat in valid_values_for_false:
-                self.start = False
-
-        return self
