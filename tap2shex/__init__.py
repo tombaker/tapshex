5314e8c062cd7159adb6f232ae4b86b6743b8b00
Tom Baker <tom@tombaker.org>
2022-05-23 16:36:05 +0200

    example02

--- tap2shex/__init__.py
+++ tap2shex/__init__.py
@@ -1,6 +1,6 @@
 """Generate ShEx from DCTAP instances."""
 
-from dctap.tapclasses import TAPShape, TAPStatementConstraint
+from dctap.tapclasses import TAPShape, TAPStatementTemplate
 from dctap.csvreader import csvreader
 
 __version__ = "0.2"
