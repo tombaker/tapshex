3d8fd96f599248fc3f4853e5a00af3cbc3b4469e
Tom Baker <tom@tombaker.org>
2022-12-28 15:53:23 +0100

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+"""Fixtures for this directory."""
+
+import pytest
+from dctap.config import get_config
+
+prefixes = """\
+prefixes:
+    ":":        "http://example.org/"
+    "dc:":      "http://purl.org/dc/elements/1.1/"
+    "dcterms:": "http://purl.org/dc/terms/"
+    "dct:":     "http://purl.org/dc/terms/"
+    "foaf:":    "http://xmlns.com/foaf/0.1/"
+    "owl:":     "http://www.w3.org/2002/07/owl#"
+    "rdf:":     "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    "rdfs:":    "http://www.w3.org/2000/01/rdf-schema#"
+    "schema:":  "http://schema.org/"
+    "skos:":    "http://www.w3.org/2004/02/skos/core#"
+    "skosxl:":  "http://www.w3.org/2008/05/skos-xl#"
+    "wdt:":     "http://www.wikidata.org/prop/direct/"
+    "xsd:":     "http://www.w3.org/2001/XMLSchema#"
+    "ex":       "http://ex.example/#"
+    "xsd":      "http://www.w3.org/2001/XMLSchema#"
+    "school":   "http://school.example/#"
+"""
+
+@pytest.fixture(scope="session")
+def config_dict():
+    """Return a dictionary of configuration settings."""
+    return get_config(config_yamldoc=prefixes)
