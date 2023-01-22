5d76880ffc2cd9842cbe4da5fb071a63ed02d911
Tom Baker <tom@tombaker.org>
2023-01-22 19:12:41 +0100

    replace pytest.ini with section in pyproject.toml

--- tests/schema1_0_1/test_csv2json.py
+++ tests/schema1_0_1/test_csv2json.py
@@ -1,17 +1,28 @@
 """Tests for tapshex.csvreader."""
 
+import os
 from pathlib import Path
+import pytest
+from dctap.csvreader import csvreader
+from tapshex.classes import Shape, StatementTemplate
 from tapshex.config import tapshex_config
 from tapshex.csvreader import tapshex_csvreader
 
-
-def test_csv2json():
+def test_csv2json(capsys):
     """From open CSV file, convert to JSON."""
+    heredir = Path(__file__).resolve().parent 
     config_dict = tapshex_config()
-    csvfile_obj = Path("dctap.csv").open(encoding="utf-8")
-    json_obj = tapshex_csvreader(csvfile_obj, config_dict)
-    print(json_obj)
-    print(json_obj)
-    print(json_obj)
-    print(json_obj)
-    print(json_obj)
+    csvfile_obj = Path(heredir).joinpath("dctap.csv").open(encoding="utf-8")
+    # json_obj = tapshex_csvreader(csvfile_obj, config_dict)
+    json_obj = csvreader(
+        open_csvfile_obj=csvfile_obj, 
+        config_dict=config_dict,
+        shape_class=Shape,
+        state_class=StatementTemplate,
+    )
+    assert Path(heredir).joinpath("dctap.csv").exists()
+    with capsys.disabled():
+        print()
+        print(heredir)
+        print(Path(heredir).joinpath("dctap.csv"))
+        print(Path(Path(heredir)))
