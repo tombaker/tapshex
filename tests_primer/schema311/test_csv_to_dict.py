54c19873b17f7ac29a4d0042d69dc6d9df10b8f3
Tom Baker <tom@tombaker.org>
2023-01-31 11:39:15 +0100

    tests for Primer 3.1 Node Constraints - literal datatype

---
+++ tests_primer/schema311/test_csv_to_dict.py
@@ -0,0 +1,71 @@
+"""
+Convert from CSV string to Python dict:
+- uses Jinja template at ../../tapshex/template.py
+- 3.1 Node Constraints: literal datatype
+  - A node constraint that identifies the datatype of an RDF literal.
+  - https://shexspec.github.io/primer/#nodeConstraints
+"""
+
+# pylint: disable=unused-import,unused-argument,import-error
+import pytest
+from tapshex.classes import Shape, StatementTemplate
+from tapshex.config import tapshex_config
+from tapshex.csvreader import tapshex_csvreader
+
+NONDEFAULT_CONFIGYAML_STR = """
+prefixes:
+    "my:":      "http://my.example/#"
+    "xsd:":     "http://www.w3.org/2001/XMLSchema#"
+    "foaf:":    "http://xmlns.com/foaf/0.1/"
+"""
+
+
+def test_csv_to_dict(capsys):
+    """From CSV to Python dict (actual_dict)."""
+    config_dict = tapshex_config(nondefault_configyaml_str=NONDEFAULT_CONFIGYAML_STR)
+    # fmt: off
+    #
+    csvfile_str = """\
+    shapeID      , propertyID , valueDataType
+    my:UserShape , foaf:name  , xsd:string
+    """
+    #
+    # fmt: on
+    expected_dict = {
+        "namespaces": {
+            "my:": "http://my.example/#",
+            "foaf:": "http://xmlns.com/foaf/0.1/",
+            "xsd:": "http://www.w3.org/2001/XMLSchema#",
+        },
+        "shapes": [
+            {
+                "shapeID": "my:UserShape",
+                "statement_templates": [
+                    {
+                        "propertyID": "foaf:name",
+                        "valueDataType": "xsd:string",
+                    },
+                ],
+            }
+        ],
+        "warnings": {"my:UserShape": {}},
+    }
+    # pylint: disable=invalid-name
+    actual_dict = tapshex_csvreader(
+        csvfile_str=csvfile_str,
+        config_dict=config_dict,
+        shape_class=Shape,
+        state_class=StatementTemplate,
+    )
+    assert isinstance(actual_dict, dict)
+    assert isinstance(actual_dict["namespaces"], dict)
+    assert actual_dict["namespaces"] == expected_dict["namespaces"]
+    assert sorted(actual_dict["shapes"]) == sorted(expected_dict["shapes"])
+    assert actual_dict == expected_dict
+    # with capsys.disabled():
+    #     from pprint import pprint
+    #     print(Shape)
+    #     # pprint(config_dict)
+    #     # pprint(csvfile_str)
+    #     # pprint(config_dict["prefixes"])
+    #     pprint(f'Output: {actual_dict}')
