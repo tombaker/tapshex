f9140833b010a238687c5c5ebb44861641643ce4
Tom Baker <tom@tombaker.org>
2023-02-06 09:32:11 +0100

    template.py - CLOSED

--- tapshex/template.py
+++ tapshex/template.py
@@ -7,33 +7,39 @@ PREFIX {{prefix}} <{{uri}}>
 {%- endfor %}
 
 {%- for shape in shapes %}
-{{shape.shapeID}} {
+{{shape.shapeID}}{{" "}}
+  {%- if shape.closed == 'True' -%} 
+    {{"CLOSED"}} 
+  {%- elif shape.extra is defined -%}
+    {{"EXTRA "}}{{shape.extra}}
+  {%- endif -%}
+    {{" {"}}
   {%- for statement in shape.statement_templates %}
-  {{statement.propertyID}}
-  {%- if statement.valueNodeType %} {{statement.valueNodeType|upper}}{% endif -%}
-  {%- if statement.valueDataType %} {{statement.valueDataType}}{% endif -%}
-  {%- if statement.valueShape %} @{{statement.valueShape}}{% endif -%}
-  {%- if statement.valueConstraint is defined -%} 
-        {%- if not statement.valueConstraintType is defined -%}
-            {{" "}}[{{ statement.valueConstraint }}]
-        {%- elif statement.valueConstraintType == 'picklist_quoted' -%}
-            {{" "}}[{{ statement.valueConstraint | map('tojson') | join(" ") }}]
-        {%- elif statement.valueConstraintType == "picklist" -%}
-            {{" "}}[{{ statement.valueConstraint | join(" ") }}]
-        {%- endif -%}
-  {%- endif -%}
-  {%- if statement.minoccurs is defined %} { {{- statement.minoccurs -}}
-        {%- if statement.maxoccurs is not defined -%}
-            ,}
-		{%- elif statement.maxoccurs|int == -1 -%}
-            ,}
-		{%- else -%}
-		 ,{{statement.maxoccurs}}}
-		{%- endif -%} 
-  {%- endif -%}
-  {%- if statement.mininclusive %} MinInclusive {{statement.mininclusive}} {%- endif -%}
-  {%- if statement.maxinclusive %} MaxInclusive {{statement.maxinclusive}} {%- endif -%}
-  {%- if not loop.last -%} ;{%- endif -%}
+    {{statement.propertyID}}
+    {%- if statement.valueNodeType %} {{statement.valueNodeType|upper}}{% endif -%}
+    {%- if statement.valueDataType %} {{statement.valueDataType}}{% endif -%}
+    {%- if statement.valueShape %} @{{statement.valueShape}}{% endif -%}
+    {%- if statement.valueConstraint is defined -%} 
+          {%- if not statement.valueConstraintType is defined -%}
+              {{" "}}[{{ statement.valueConstraint }}]
+          {%- elif statement.valueConstraintType == 'picklist_quoted' -%}
+              {{" "}}[{{ statement.valueConstraint | map('tojson') | join(" ") }}]
+          {%- elif statement.valueConstraintType == "picklist" -%}
+              {{" "}}[{{ statement.valueConstraint | join(" ") }}]
+          {%- endif -%}
+    {%- endif -%}
+    {%- if statement.minoccurs is defined %} { {{- statement.minoccurs -}}
+          {%- if statement.maxoccurs is not defined -%}
+              ,}
+      	{%- elif statement.maxoccurs|int == -1 -%}
+              ,}
+      	{%- else -%}
+      	 ,{{statement.maxoccurs}}}
+      	{%- endif -%} 
+    {%- endif -%}
+    {%- if statement.mininclusive %} MinInclusive {{statement.mininclusive}} {%- endif -%}
+    {%- if statement.maxinclusive %} MaxInclusive {{statement.maxinclusive}} {%- endif -%}
+    {%- if not loop.last -%} ;{%- endif -%}
   {%- endfor %}
 }
 {%- endfor %}
